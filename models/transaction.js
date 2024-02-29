01640fad4c991643150af0e2bc0f90c23a8dcc2d
heru <jrs.herupranata@gmail.com>
2024-03-01 00:03:15 +0700

    creating first model

---
+++ models/transaction.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Transaction.init({
+    invoice_number: DataTypes.STRING,
+    transaction_type: DataTypes.STRING,
+    description: DataTypes.STRING,
+    total_amount: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Transaction',
+  });
+  return Transaction;
+};
